7de47107133bd6b4fe7afe1649e16cf6003a487f
LimSujin0 <imsujin0@naver.com>
2018-04-03 02:22:37 +0900

    Add YearToCentury

--- src/main/java/edu/handong/java/csee/prob1/YearToCentury.java
+++ src/main/java/edu/handong/java/csee/prob1/YearToCentury.java
@@ -1,5 +1,22 @@
 package edu.handong.java.csee.prob1;
 
 public class YearToCentury {
-
+	int year;
+	
+	public YearToCentury() {
+		year = 0;
+	}
+	
+	public YearToCentury(int year) {
+		this.year = year;
+	}
+	
+	public int calc_century() {
+		if(year%100 == 0)
+			year = year/100;
+		else {
+			year = (year/100)+1;
+		}
+		return year;
+	}
 }
--- src/main/java/edu/handong/java/csee/prob1/YearToCentury_main.java
+++ src/main/java/edu/handong/java/csee/prob1/YearToCentury_main.java
@@ -1,5 +1,19 @@
-package edu.handong.java.csee.prob1;
+package edu.handong.java.csee.prob1;//package name
+
+import java.util.Scanner;//import Scanner class
 
 public class YearToCentury_main {
+	
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);//Instantiating the scanner
+		int yr = 0; //this is variable named yr
+		
+		System.out.println("Input year: "); //print out this line on the monitor
+		yr = keyboard.nextInt(); //get a integer variable from the user, and set yr to getting num.
+		
+		YearToCentury year = new YearToCentury(yr);
+		System.out.println(yr + "is" + year.calc_century() + "th century. ");
+		
+	}
 
 }
