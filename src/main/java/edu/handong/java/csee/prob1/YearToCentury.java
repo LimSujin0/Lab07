1909fb894f6388c3c8bf785c0258f4ee51c19fa1
LimSujin0 <imsujin0@naver.com>
2018-04-03 23:42:52 +0900

    complete YearToCentury's comments

--- src/main/java/edu/handong/java/csee/prob1/YearToCentury.java
+++ src/main/java/edu/handong/java/csee/prob1/YearToCentury.java
@@ -1,21 +1,41 @@
-package edu.handong.java.csee.prob1;
+package edu.handong.java.csee.prob1;//package name
 
+/**
+ * This is class named YearToCentury
+ * this class defines variables, methods for the program
+ * @author imsuj
+ *
+ */
 public class YearToCentury {
-	int year;
+	int year;//this is integer variable named year
 	
+	/**
+	 * This is a public method named YearToCentry
+	 * This class makes year's variable 0
+	 */
 	public YearToCentury() {
-		year = 0;
+		year = 0;//set year to 0
 	}
 	
+	/**
+	 * this is a public method named YearToCentury
+	 * this class sets year to value given from user 
+	 * @param year
+	 */
 	public YearToCentury(int year) {
-		this.year = year;
+		this.year = year;//sets year to value given from user 
 	}
 	
+	/**
+	 * This is a public method named clac-century
+	 * This method calculates a century about year
+	 * @return
+	 */
 	public int calc_century() {
-		if(year%100 == 0)
-			year = year/100;
-		else {
-			year = (year/100)+1;
+		if(year%100 == 0)//if year%100 == 0, (if year is fully divided by 100)
+			year = year/100;//set century to year/100
+		else {//if year is not fully divided by 100
+			year = (year/100)+1;//set century to year 100+1
 		}
 		return year;
 	}
--- src/main/java/edu/handong/java/csee/prob1/YearToCentury_main.java
+++ src/main/java/edu/handong/java/csee/prob1/YearToCentury_main.java
@@ -1,18 +1,28 @@
 package edu.handong.java.csee.prob1;//package name
 
 import java.util.Scanner;//import Scanner class
-
-public class YearToCentury_main {
-	
-	public static void main(String[] args) {
+/**
+ * This is a class named YearToCentury_main.
+ * this class calculates the century given a year. 
+ * @author imsuj
+ *
+ */
+public class YearToCentury_main { //class named YearToCentury_main
+	/**
+	 * This is a main method of this class
+	 * this main method receive user's input, year
+	 * and print out the outcome on the monitor
+	 * @param args
+	 */
+	public static void main(String[] args) { //main method in this program.
 		Scanner keyboard = new Scanner(System.in);//Instantiating the scanner
 		int yr = 0; //this is variable named yr
 		
 		System.out.println("Input year: "); //print out this line on the monitor
 		yr = keyboard.nextInt(); //get a integer variable from the user, and set yr to getting num.
 		
-		YearToCentury year = new YearToCentury(yr);
-		System.out.println(yr + " is " + year.calc_century() + "th century. ");
+		YearToCentury year = new YearToCentury(yr);//instantiating YearToCentury class
+		System.out.println(yr + " is " + year.calc_century() + "th century. ");//print out the outcome of the century.
 		
 	}
 
