c3c45db5db8568109d4404fcc4b46424e984f41e
LimSujin0 <imsujin0@naver.com>
2018-04-04 00:07:46 +0900

    Complete Chicken class's comments

--- src/main/java/edu/handong/java/csee/prob2/Chicken.java
+++ src/main/java/edu/handong/java/csee/prob2/Chicken.java
@@ -1,21 +1,37 @@
-package edu.handong.java.csee.prob2;
+package edu.handong.java.csee.prob2;//package name
 
+/**
+ * This is a public class named chicken
+ * This class define the variables and methods needed to this program
+ * @author imsuj
+ *
+ */
 public class Chicken {
-	private String name;
-	private double price;
-	private int stars;
+	private String name;//this is a sting type variable named name
+	private double price;//this is a double type variable named price 
+	private int stars;//this is a integer type variable named stars
 	
+	/**
+	 * This is a public method named chicken
+	 * This method Initialize the variables(name, price, stars) 
+	 */
 	public Chicken() {
-		this.name = " ";
-		this.price = 0.0;
-		this.stars = 0;
+		this.name = " ";//set name to " "(blank)
+		this.price = 0.0;//set price to 0.0
+		this.stars = 0;//set stars to 0
 	}
 	
-	public Chicken(String name, double price, int stars) {
+	/**
+	 * This is a mehotd named Chicken
+	 * @param name
+	 * @param price
+	 * @param stars
+	 */
+	/*public Chicken(String name, double price, int stars) {
 		this.name = name;
 		this.price = price;
 		this.stars = stars;
-	}
+	}*/
 	
 	public String get_name() {
 		return this.name;
--- src/main/java/edu/handong/java/csee/prob2/Chicken_main.java
+++ src/main/java/edu/handong/java/csee/prob2/Chicken_main.java
@@ -1,18 +1,30 @@
-package edu.handong.java.csee.prob2;
+package edu.handong.java.csee.prob2;//this is package name
 
+/**
+ * This is class named Chicken_main
+ * This program is print out each menu's name and rating
+ * Enjoy my program!
+ * @author imsuj
+ *
+ */
 public class Chicken_main {
-
+	
+	/**
+	 * This is a main method of Chicken_main class
+	 * This main method prints out chicken's rating
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		Chicken menu1 = new Chicken("Chesse_mustard", 16000.00, 5);
-		Chicken menu2 = new Chicken("Honey_mustard", 16000.00, 4);
-		Chicken menu3 = new Chicken("Spily_mustard", 17000.00, 1);
+		Chicken menu1 = new Chicken("Chesse_mustard", 16000.00, 5);//instantiating Chicken class( menu1)
+		Chicken menu2 = new Chicken("Honey_mustard", 16000.00, 4);//instantiating Chicken class( menu2)
+		Chicken menu3 = new Chicken("Spily_mustard", 17000.00, 1);//instantiating Chicken class( menu3)
 		
-		menu1.set_stars(4);
-		menu2.set_stars(2);
-		menu3.set_stars(5);
+		menu1.set_stars(3);//set the menu1's stars to 3
+		menu2.set_stars(4);//set the menu2's stars to 4
+		menu3.set_stars(1);//set the menu3's stars to 1
 		
-		System.out.println(menu1.get_name() + "'s rating is " + menu1.get_stars());
-		System.out.println(menu2.get_name() + "'s rating is " + menu2.get_stars());
-		System.out.println(menu3.get_name() + "'s rating is " + menu3.get_stars());
+		System.out.println(menu1.get_name() + "'s rating is " + menu1.get_stars());//print out the menu1's rating
+		System.out.println(menu2.get_name() + "'s rating is " + menu2.get_stars());//print out the menu2's rating
+		System.out.println(menu3.get_name() + "'s rating is " + menu3.get_stars());//print out the menu3's rating
 	}
 }
